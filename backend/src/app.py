2b368485ded936da5cd9095bf72f1058df22eaa7
TRAN Quoc Hung <quochungtr99@gmail.com>
2024-12-07 02:53:21 +0100

    init fastApi

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,11 @@
 import logging
-import time
-from typing import Dict, Optional
-from celery.result import AsyncResult
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from src.rag.vectordb import QdrantDatabase
+
+# import time
+# from typing import Dict, Optional
+# from celery.result import AsyncResult
+# from pydantic import BaseModel
+# from src.rag.vectordb import QdrantClient
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +14,4 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
