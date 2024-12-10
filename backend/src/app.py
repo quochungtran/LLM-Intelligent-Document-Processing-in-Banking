4e2208684582efd96f52766efa1c325ac444bae1
TRAN Quoc Hung <quochungtr99@gmail.com>
2024-12-10 21:37:08 +0100

    notebook for data and chunking analyst

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,11 +1,11 @@
 import logging
 from fastapi import FastAPI, HTTPException
 
-# import time
-# from typing import Dict, Optional
-# from celery.result import AsyncResult
-# from pydantic import BaseModel
-# from src.rag.vectordb import QdrantClient
+import time
+from typing import Dict, Optional
+from celery.result import AsyncResult
+from pydantic import BaseModel
+from src.rag.vectordb import QdrantClient
 
 logger = logging.getLogger(__name__)
 
