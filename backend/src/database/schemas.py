2f0f1100898a9fed6ea9bbe32680b77cc495836e
TRAN Quoc Hung <quochungtr99@gmail.com>
2025-02-17 02:16:26 +0100

    schemas for home loan profile

--- backend/src/database/schemas.py
+++ backend/src/database/schemas.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, func
-from sqlalchemy import Column, String, Boolean, DateTime, Integer
+from sqlalchemy import Column, String, Boolean, DateTime, Integer, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 from src.database.database import engine
@@ -19,3 +19,15 @@ class ChatConversation(Base):
     completed = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+
+class Profile(Base):
+    __tablename__ = 'homeloan_profile'
+    id   = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100))
+    income = Column(Integer)
+    loan_amount = Column(Integer)
+    loan_term = Column(Integer)
+    loan_to_value_ratio = Column(Float)
+    debt_to_income_ratio = Column(Float)
+    property_value = Column(Integer)
+    home_purpose = Column(String)
